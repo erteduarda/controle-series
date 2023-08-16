31ba8475ee07a2ebca590404ab26827e61b237a2
erteduarda <erteduarda@gmail.com>
2023-08-15 22:48:23 -0300

    removendo series da lista.

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SeriesController;
+use App\Models\Serie;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,9 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/series');
 });
 
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/criar', [SeriesController::class, 'create']);
-Route::post('/series/salvar', [SeriesController::class, 'store']);
+Route::resource('/series', SeriesController::class)
+    ->only(['index', 'create', 'store']);
+
+Route::post('/series/destroy/{series}', [SeriesController::class, 'destroy'])
+    ->name('series.destroy');
